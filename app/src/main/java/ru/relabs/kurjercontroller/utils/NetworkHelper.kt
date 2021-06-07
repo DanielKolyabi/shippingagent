24bb4c8358d3e87e7086c413e50a2e1a4a7b2df9
Daniil Kruchanov <dkurch84@gmail.com>
2021-06-07 12:50:42 +0300

    #70 Add additional way to check if mobile data enabled

--- app/src/main/java/ru/relabs/kurjercontroller/utils/NetworkHelper.kt
+++ app/src/main/java/ru/relabs/kurjercontroller/utils/NetworkHelper.kt
@@ -6,18 +6,7 @@ import android.location.LocationManager.GPS_PROVIDER
 import android.net.ConnectivityManager
 import android.net.wifi.WifiManager
 import android.provider.Settings
-import android.util.Log
-import com.google.android.gms.common.api.GoogleApiClient
-import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.location.LocationSettingsRequest
-import com.google.android.gms.location.LocationSettingsStatusCodes
-import ru.relabs.kurjercontroller.presentation.host.HostActivity
-import ru.relabs.kurjercontroller.presentation.host.featureCheckers.REQUEST_LOCATION
-import java.io.File
-import java.io.FileOutputStream
-import java.net.HttpURLConnection
-import java.net.URL
+import java.lang.reflect.Method
 
 
 /**
@@ -45,21 +34,34 @@ object NetworkHelper {
         return wifiManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).isConnected
     }
 
+    private fun isMobileDataEnabledReflect(context: Context?): Boolean {
+        context ?: return false
+
+        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        return try {
+            val cmClass = Class.forName(cm.javaClass.name)
+            val method: Method = cmClass.getDeclaredMethod("getMobileDataEnabled")
+            method.isAccessible = true // Make the method callable
+            // get the setting for "mobile data"
+            method.invoke(cm) as Boolean
+        } catch (e: Exception) {
+            false
+        }
+    }
+
     private fun isMobileDataEnabled(context: Context?): Boolean {
         context ?: return false
+
         val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
-        return Settings.Secure.getInt(context.contentResolver, "mobile_data", 0) == 1 || cm.getNetworkInfo(
-            ConnectivityManager.TYPE_MOBILE
-        ).isConnectedOrConnecting
+        return Settings.Secure.getInt(context.contentResolver, "mobile_data", 0) == 1 ||
+                cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).isConnectedOrConnecting ||
+                isMobileDataEnabledReflect(context)
     }
 
     fun isNetworkEnabled(context: Context?): Boolean {
-        return ((isWifiEnabled(context) && isWifiConnected(
-            context
-        )) || isMobileDataEnabled(context)) && !isAirplaneModeEnabled(
-            context
-        )
+        return ((isWifiEnabled(context) && isWifiConnected(context)) || isMobileDataEnabled(context))
+                && !isAirplaneModeEnabled(context)
     }
 
 
@@ -72,7 +74,7 @@ object NetworkHelper {
         )
     }
 
-    fun isGPSEnabled(context: Context?): Boolean{
+    fun isGPSEnabled(context: Context?): Boolean {
         return (context?.getSystemService(Context.LOCATION_SERVICE) as? LocationManager)?.isProviderEnabled(GPS_PROVIDER) ?: false
     }
 }
