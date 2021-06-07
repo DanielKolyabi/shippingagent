e255baa759655b97cd6581ca18992fcd57b4a04f
Daniil Kruchanov <dkurch84@gmail.com>
2021-06-07 12:51:22 +0300

    Bump to 54

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "ru.relabs.kurjercontroller"
         minSdkVersion(21)
         targetSdkVersion(29)
-        versionCode = 53
-        versionName = "53"
+        versionCode = 54
+        versionName = "54"
 
         buildConfigField("int", "MAX_ADDRESSES_IN_FILTERS", "500")
         buildConfigField("String", "YA_KEY", "\"9be78b32-1265-4157-bf15-999b8d747a5c\"")
