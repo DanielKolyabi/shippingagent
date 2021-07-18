8aa08530046865fce57a69cd5487874e3f55fbf9
Daniil Kruchanov <dkurch84@gmail.com>
2021-07-18 19:18:39 +0300

    Bump to 57

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "ru.relabs.kurjercontroller"
         minSdkVersion(21)
         targetSdkVersion(29)
-        versionCode = 56
-        versionName = "56"
+        versionCode = 57
+        versionName = "57"
 
         buildConfigField("Boolean", "FEATURE_PHOTO_RADIUS", "false")
         buildConfigField("Boolean", "DISABLE_GPS_NETWORK_CHECK", "false")
