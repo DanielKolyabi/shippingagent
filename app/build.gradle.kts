c5c26aefd03ca07d9c5c6bf9b596bb1aea38ec2b
Daniil Kruchanov <dkurch84@gmail.com>
2021-09-28 13:45:21 +0300

    Bump to 64

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "ru.relabs.kurjercontroller"
         minSdkVersion(21)
         targetSdkVersion(29)
-        versionCode = 63
-        versionName = "63"
+        versionCode = 64
+        versionName = "64"
 
         buildConfigField("Boolean", "FEATURE_PHOTO_RADIUS", "false")
         buildConfigField("Boolean", "DISABLE_GPS_NETWORK_CHECK", "false")
