c701882b6fb6fcb1076de803134468d15693f7a6
terokon <terokon59@gmail.com>
2023-09-14 14:28:28 +0300

    Версия 86

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "ru.relabs.kurjercontroller"
         minSdkVersion(21)
         targetSdkVersion(29)
-        versionCode = 85
-        versionName = "85"
+        versionCode = 86
+        versionName = "86"
 
         buildConfigField("Boolean", "FEATURE_PHOTO_RADIUS", "false")
         buildConfigField("Boolean", "DISABLE_GPS_NETWORK_CHECK", "false")
