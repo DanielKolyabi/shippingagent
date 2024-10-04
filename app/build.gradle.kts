3444574d18a17d8d5acdb64672cb5b299ee89602
daniil.kolyabin <kolabin14@gmail.com>
2024-10-04 13:51:10 +0300

    12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "ru.relabs.kurjercontroller"
         minSdkVersion(21)
         targetSdkVersion(29)
-        versionCode = 88
-        versionName = "88"
+        versionCode = 89
+        versionName = "89"
 
         buildConfigField("Boolean", "FEATURE_PHOTO_RADIUS", "false")
         buildConfigField("Boolean", "DISABLE_GPS_NETWORK_CHECK", "false")
